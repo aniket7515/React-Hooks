53d9c4e21d925e18ed5a92814f9a921e6af20996
Aniket <aniketdhokane2071@gmail.com>
2022-01-13 17:31:04 +0530

    UseEffect

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import ReducerTutorial from './ReducerTutorial';
 import StateTutorial from './StateTutorial'
+import EffectTutorial from './EffectTutorial'
 
 function App() {
   return (
     <div className="App">
       <StateTutorial/>
       <ReducerTutorial/>
+      <EffectTutorial/>
     </div>
   );
 }
