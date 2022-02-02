dc117f3ca42f4c621405359a63a1efdbbd49a2d0
Aniket <aniketdhokane2071@gmail.com>
2022-02-02 16:52:45 +0530

    UseEffect after render

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import ReducerTutorial from './ReducerTutorial';
 import StateTutorial from './StateTutorial'
 import EffectTutorial from './EffectTutorial'
 import HookCounterThree from './components/HookCounterThree';
-import HookCounterFour from './components/HookCounterFour';
+// import HookCounterFour from './components/HookCounterFour';
+import HookCounterOne from './components/HookCounterOne';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <ReducerTutorial/>
       <EffectTutorial/> */}
       {/* <HookCounterThree/> */}
-      <HookCounterFour/>
+      {/* <HookCounterFour/> */}
+      <HookCounterOne/>
     </div>
   );
 }
